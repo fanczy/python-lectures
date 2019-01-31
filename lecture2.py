d4f750ca64f8e7b0f8b05666b164ab49d44c7a89
fanczy <fanczy@gmail.com>
2019-01-31 22:07:39 +0100

    lecture 2 while and excercises

--- lecture2.py
+++ lecture2.py
@@ -1,17 +1,33 @@
 print("===============")
 
-##### HOMEWORK #####
+##### LAST HOMEWORK #####
 # check if two vectors are the same length
 # check if words are anagrams
 ######################
+
+######################
+##### CONDITIONS #####
+######################
+
+if True: # if condition in statement is true then nested code is executed
+    print("It's true") # code executed in condition must be nested
+
+condition = False
+
+if condition:
+    print("It's true")
+else: # else nested code is executed if the condition is false
+    print("It's false")
+
 
 wordA = "ward"
 wordB = "draw"
 
 if set(wordA) == set(wordB):
-    print(f"Words {wordA} and {wordB} are anagrams") # code executed in condition must be nested
+    print(f"Words {wordA} and {wordB} are anagrams")
 else:
     print(f"Words {wordA} and {wordB} are not anagrams")
+
 
 a = 5
 b = 6
@@ -20,10 +36,27 @@ if a > b:
     print(f"{a} is bigger than {b}")
 elif a == b:
     print(f"{a} is equal to {b}")
-else: # all happens if all predecessing conditions are false
+else: # happens if all predecessing conditions are false
     print(f"{a} is smaller than {b}")
 
+
+containment_phrase = "contains"
+
+if "b" not in wordA:
+    containment_phrase = "does not contain"
+
+print(f"Word '{wordA}' {containment_phrase} letter 'b'" )
+
+##### excercises #####
+# two variables: a, b write a code that will put bigger value to a and lower to b
+######################
+
+#####################
+##### FOR LOOPS #####
+#####################
+
 numbers = [0, 1, 2, 3, 4, 5]
+
 for number in numbers: # executes nested code for every value in iterable (iterables are things that you can iterate over)
     print(number)
 
@@ -31,34 +64,32 @@ for number in numbers:
     if number % 2 == 0:
         print(f"{number} is even")
 
+for number in numbers:
+    if number > 2:
+        break # breaks execution of this for loop
+    print(number)
+
+for number in numbers:
+    if number == 2:
+        continue # skips the rest of nested code and continues on another value
+    print(number)
+
+
 sum = 0
 for number in numbers:
     sum += number
 
 print(f"Sum of {numbers} is {sum}") 
 
-for i in range(10): # zero to nine
-    print(i)
-
-for i in range(5, 10):
-    print(i)
-
-for i in range(10, 20, 2):
-    print(i)
-
-for i in range (10, 0, -1):
-    print(i)
+##### excercises #####
+# find min and max in list of values | float("inf") | float("-inf") <- biggest and lowest float
+# in list of strings print first word that contains letter "a"
+######################
 
 word = "Hello"
 
 for letter in word:
     print(letter)
-
-for index in range(len(word)):
-    print(word[index])
-
-for index in range(len(word)):
-    print(word[-1 - index])
 
 # vectors = [[1,2], [3,4], [5,6]]
 vectors = [(1,2), (3,4), (5,6)]
@@ -74,8 +105,6 @@ store_prices = {
     "pizza": 4.20
 }
 
-
-
 for item in store_prices: # by default for iterates over dictionary keys, iterating over dictionary is not in order
     print(item)
 
@@ -87,6 +116,81 @@ for item in store_prices.items():
 
 for item, price in store_prices.items():
     print(f"{item} costs {price}")
+
+
+for i in range(10): # zero to nine
+    print(i)
+
+for i in range(5, 10):
+    print(i)
+
+for i in range(10, 20, 2):
+    print(i)
+
+for i in range (10, 0, -1):
+    print(i)
+
+for index in range(len(word)):
+    print(word[index])
+
+for index in range(len(word)):
+    print(word[-1 - index])
+
+print(range(5))
+print(list(range(5)))
+
+for index, letter in enumerate(word): # enumerates items with index
+    print(index, letter)
+
+##### excercises #####
+# print 7!
+# menu = {
+#     "soups": {
+#         "tomato soup": 25,
+#         "chicken soup": 20
+#     },
+#     "main courses": {
+#         "spaghetti bolognese": 80,
+#         "pineapple pizza": 420
+#     },
+#     "deserts": {
+#         "ice cream": 30,
+#         "brownies": 30 
+#     },
+#     "drinks": {
+#         "beer": 5,
+#         "water": 100
+#     }
+# } 
+# print in human readable form
+# ===== soups =====
+# tomato soup .... 25 BTC
+# chicken soup ..... 20 BTC
+# ===== main courses =====
+# spaghetti bolognese ..... 80 BTC
+# .
+# .
+# .
+######################
+
+counter = 0
+
+while counter < 10: # executes nested code while initial condition is still True
+    print(counter)
+    counter += 1
+
+while counter > 0:
+    print(counter)
+    counter -= 1
+else:
+    print(f"Counter stopped at {counter}.")
+
+
+##### excercises #####
+# print random numbers from 0 to 10 until it's 7, then print how many tries did it take to hit 7 | from random import randint <- imports random number generator | randint(0,10)
+######################
+
+
 
 
 
