fcbdc203bbf2f38a55cff9db70fc57d4e532711c
Silný Tomáš <silny@colsys.cz>
2019-02-22 16:27:17 +0100

    Added example of function returning value

--- lecture3.py
+++ lecture3.py
@@ -9,10 +9,18 @@ greet("Person")
 
 # greet() Will throw a error, because it's required argument
 
-def greet(name="Person"): # now the argument name has default value "Person" that will be used if this argument is not filled.
+def greet(name="Person"): # Now the argument name has default value "Person" that will be used if this argument is not filled.
     print(f"Hello, {name}.")
 
 greet()
+
+def give_answer(question):
+    return 42 # Functions can return value that can be used later or stored in variable.
+
+answer = give_answer("Ultimate question of life, the universe, and everything")
+
+print(answer)
+
 
 ###########################
 ###### DOCUMENTATION ######
