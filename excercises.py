6432e454114d7d60c0a471fc4da760d0b89d5325
Tomas Silny <silny@silny.Colsys>
2019-03-05 09:49:17 +0100

    excercises

---
+++ excercises.py
@@ -0,0 +1,37 @@
+# Write a function that takes name as argument
+# That function prints "Hello, " and the name in uppercase and reversed order
+# example: reverse_greet("Karel")
+# writes "Hello, LERAK."
+
+# Write a function that takes list of strings as argument
+# That function returns list of strings that conatin letter "a" in alphabetical order
+# example: contains_a_alphabetically(["zada", "noha", "bricho", "hlava"])
+# returns ["hlava", "noha", "zada"]
+
+
+# Write a function that takes a string as argument
+# This function returns dictionary of letters contained in the string a keys
+# Values in this dictionary will number be occurences of the letters
+# example: count_letter_occurences("Level")
+# returns { l: 2, e: 2, v: 1}
+
+# Write a function that takes one number and one optional list as arguments
+# This function generates sequence of random numbers between 0 and 9 of set length
+# First argument is length of sequence
+# Second argument is optional (function can be called without it) list of forbidden numbers 
+# Forbidden numbers will not be in the generated sequence
+# example: random_sequence(5)
+# returns [2,3,9,1,4]
+# example: random_sequence(5,[1,2,3,4,5,6,7,8,9])
+# returns [0,0,0,0,0]
+# bonus: if sequence gets all possible numbers as forbidden numbers, print warning and return empty list
+
+# Write a function that takes n booleans as argument
+# This function returns boolean that is result of AND operation of all these values passed as a argument
+# example: multiand(True, True, False, True, False, True)
+# returns False
+# example: multiand(True, True, True)
+# returns True
+
+# Same as above but with OR instead of AND
+
